dbfb913eb3cf775bfd145da368e74f18c4f4ca50
Aeyran-Dapouyeh <ariandapoyeh@gmail.com>
2021-11-12 14:29:25 +0100

    Regression added

---
+++ LOGISTICREG.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 14:27:28 2021
+
+@author: arian
+"""
+
+# %% Import packages/libraries
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import model_selection, tree
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import scipy
+from scipy.stats import kstest, norm, zscore
+import statsmodels.api as sm
+import pylab
+from toolbox_02450 import feature_selector_lr, bmplot
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from toolbox_02450 import train_neural_net, draw_neural_net, visualize_decision_boundary
+import torch
+
+
+from matplotlib.pyplot import figure, plot, subplot, title, xlabel, ylabel, show, clim, hist, legend, ylim
+import sklearn.linear_model as lm
+
+# %% Import data
+
+# Read
+#my_data = np.genfromtxt('heart.csv', names=header, delimiter=',')
+df=pd.read_csv('heart.csv')
+
+# Split
+data=df.loc[:,'age':'thal']
+
+
+N, M = df.shape
+
+X = df.to_numpy()
+y=(df.iloc[:,-1]).to_numpy()
+y.shape = (y.shape[0], 1)
+
+
+attributeNames = list(data.columns)
+
+
+# %% Inspired by 8.1.2
+
+
+# Create crossvalidation partition for evaluation
+# using stratification and 95 pct. split between training and test 
+K = 20
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.95, stratify=y)
+# Try to change the test_size to e.g. 50 % and 99 % - how does that change the 
+# effect of regularization? How does differetn runs of  test_size=.99 compare 
+# to eachother?
+
+# Standardize the training and set set based on training set mean and std
+mu = np.mean(X_train, 0)
+sigma = np.std(X_train, 0)
+
+X_train = (X_train - mu) / sigma
+X_test = (X_test - mu) / sigma
+
+
+
+# %%
+
+# Fit regularized logistic regression model to training data to predict 
+# the type of wine
+lambda_interval = np.logspace(-8, 2, 50)
+train_error_rate = np.zeros(len(lambda_interval))
+test_error_rate = np.zeros(len(lambda_interval))
+coefficient_norm = np.zeros(len(lambda_interval))
+for k in range(0, len(lambda_interval)):
+    mdl = LogisticRegression(penalty='l2', C=1/lambda_interval[k] )
+    
+    mdl.fit(X_train, y_train)
+
+    y_train_est = mdl.predict(X_train).T
+    y_test_est = mdl.predict(X_test).T
+    
+    train_error_rate[k] = np.sum(y_train_est != y_train) / len(y_train)
+    test_error_rate[k] = np.sum(y_test_est != y_test) / len(y_test)
+
+    w_est = mdl.coef_[0] 
+    coefficient_norm[k] = np.sqrt(np.sum(w_est**2))
+
+min_error = np.min(test_error_rate)
+opt_lambda_idx = np.argmin(test_error_rate)
+opt_lambda = lambda_interval[opt_lambda_idx]
+
+
+# %%
+
+
+plt.figure(figsize=(8,8))
+#plt.plot(np.log10(lambda_interval), train_error_rate*100)
+#plt.plot(np.log10(lambda_interval), test_error_rate*100)
+#plt.plot(np.log10(opt_lambda), min_error*100, 'o')
+plt.semilogx(lambda_interval, train_error_rate*100)
+plt.semilogx(lambda_interval, test_error_rate*100)
+plt.semilogx(opt_lambda, min_error*100, 'o')
+plt.text(1e-8, 3, "Minimum test error: " + str(np.round(min_error*100,2)) + ' % at 1e' + str(np.round(np.log10(opt_lambda),2)))
+plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
+plt.ylabel('Error rate (%)')
+plt.title('Classification error')
+plt.legend(['Training error','Test error','Test minimum'],loc='upper right')
+plt.ylim([0, 4])
+plt.grid()
+plt.show()    
+
+plt.figure(figsize=(8,8))
+plt.semilogx(lambda_interval, coefficient_norm,'k')
+plt.ylabel('L2 Norm')
+plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
+plt.title('Parameter vector L2 norm')
+plt.grid()
+plt.show()    
+
